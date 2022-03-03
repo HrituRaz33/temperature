2b08a8b17023ed7b78339f536c94ca6c0de83f0a
HrituRaz33 <95464215+HrituRaz33@users.noreply.github.com>
2022-03-04 01:26:30 +0600

    api console log done i get some data

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ const API_KEY =`58cf9dd1b803485bf3014d264e77d1bc`;
 const sraechTemparature= () =>{
     const city = document.getElementById('city-name').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data=>displayTemparature(data));
+}
+
+const displayTemparature =(temparature)=>{
+    console.log(temparature);
 }
